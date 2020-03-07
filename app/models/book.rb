50894270a76dfa1b41e8531fc9bec219b8dc1619
yujiro fujiwara <imp.yfujiwara@gmail.com>
2020-03-07 12:04:48 +0900

    model/book_specの要件を実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
+
+  # Associations
   belongs_to :user
+
+  # Validates
+  validates :title,
+            presence: true
+  validates :body,
+            presence: true,
+            length: { maximum: 200 }
+
 end
