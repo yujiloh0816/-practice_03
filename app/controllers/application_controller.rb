89c01049691736fd3b1a50abd333f71bf8900c77
yujiro fujiwara <imp.yfujiwara@gmail.com>
2020-03-07 17:38:38 +0900

    トップページのテストの要件を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
-
+  before_action :authenticate_user!, unless: :home_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 
   protected
 
@@ -8,4 +12,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def home_controller?
+    params[:controller] == 'home'
+  end
 end
