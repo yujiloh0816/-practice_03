c452cd4fa5ffd1102f8ad3494b42b08c4dfdb3f6
yujiro fujiwara <imp.yfujiwara@gmail.com>
2020-03-07 12:01:07 +0900

    deviseを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #Validates
+  validates :name,
+            presence: true,
+            length: { minimum: 2, maximum: 20 },
+            uniqueness: true
+
+  validates :email,
+            presence: true,
+            uniqueness: true
+
 end
