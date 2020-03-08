4bf16c2cfb790ce72767a40a15c32b6020b764d7
yujiro fujiwara <imp.yfujiwara@gmail.com>
2020-03-08 11:12:43 +0900

    boostrapのテストの要件とレイアウトを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,11 +13,6 @@ class BooksController < ApplicationController
   def show
   end
 
-  # GET /books/new
-  def new
-    @book = Book.new
-  end
-
   # GET /books/1/edit
   def edit
   end
@@ -26,13 +21,14 @@ class BooksController < ApplicationController
   # POST /books.json
   def create
     @book = current_user.books.build(book_params)
+    @books = Book.all
 
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
       else
-        format.html { render :new }
+        format.html { render :index }
         format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get 'home/about'
-  resources :books
+  resources :books, except: [:new]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
