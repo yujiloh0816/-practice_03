023341284895dd78de52fa961b8f69f8dc9d7c67
yujiro fujiwara <imp.yfujiwara@gmail.com>
2020-03-08 11:32:22 +0900

    不要なアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   get 'home/about'
   resources :books, except: [:new]
-  resources :users
+  resources :users, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
